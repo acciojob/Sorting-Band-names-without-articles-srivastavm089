2b5826f8d3b916a8c5694dc76a0a2425b6edc68d
Abhay kumar srivastava <120777323+srivastavm089@users.noreply.github.com>
2023-04-17 17:47:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 //your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let updatedList = arr.map((element)=>{
+	let words = element.split(' ');
+	let updateString = words.reduce((prev,current,index)=>{
+		if(current !="The" && current !='an' && current !='a'){
+			return prev + ' '+current
+		}
+		return prev;
+	},'')
+	return updateString
+});
+let mp ={};
+updatedList.forEach((element,index)=>{
+	mp[element] = arr[i];
+})
+updatedList.sort();
+let finalAns = updatedList.map((element)=>{
+	return mp[element]
+})
+console.log(finalAns)
 
-const a = touristSpots.sort();
-console.log(a)
